d895ecb71d9b554b6d97cc819774bd044535309f
Harry White <harry.white424@gmail.com>
2020-04-09 16:04:05 +1000

    Mockup of dashboard with vue components

---
+++ src/timeSeries/components/MetadataDisplay.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="placeholder-metadata">
+        <p>Metadata display goes here</p>
+    </div>
+</template>
+
+<style>
+.placeholder-metadata {
+    width: 100%;
+    height: 300px;
+    background-color: gray;
+}
+
+.placeholder-metadata p {
+    text-align: center;
+    padding-top: 100px;
+}
+</style>
