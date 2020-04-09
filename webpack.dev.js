966cd9d5344775fdda66fe177d609dcc196fcd06
Harry White <harry.white424@gmail.com>
2020-04-09 16:39:03 +1000

    Vue mockup and dev environment setup (#2)
    * Setup initial vue component for timeSeries
    
    * Webpack config for vue loading and development
    
    * Ignore node_modules and dist
    
    * Add development server instructions to readme
    
    * Mockup of dashboard with vue components
    
    * Fix path to root component
    
    * Add window title

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,19 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
+const path = require('path');
 
 module.exports = merge(common, {
-        mode: 'development',
-        devtool: 'inline-source-map',
-        devServer: {
-            contentBase: './dist'
-        }
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['vue-style-loader', 'css-loader']
+            },
+        ]
     }
-);
+});
