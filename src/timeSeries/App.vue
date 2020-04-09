f465a53537e9d1c2c62cefdcba4b8f91bb9da485
Harry White <harry.white424@gmail.com>
2020-04-09 15:15:29 +1000

    Setup initial vue component for timeSeries

---
+++ src/timeSeries/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Hello {{ name }}</h1>    
+</template>
+
+<script>
+export default {
+    data: () => ({
+       'name': 'World'
+    })
+}
+</script>
+
+<style>
+h1 {
+    border-bottom: 1px solid steelblue;
+}
+</style>
