0925bde06bd1e4b1b153dd014386640beb5f61fa
Harry White <harry.white424@gmail.com>
2020-04-09 15:17:06 +1000

    Webpack config for vue loading and development

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
 const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const fs = require('fs');
 
 
@@ -12,21 +14,14 @@ module.exports = {
         "covidTimeSeries": './src/timeSeries/index.js'
     },
     plugins: [
-        new MiniCssExtractPlugin({
-            filename: './src/timeSeries/timeSeries.css'
-        }),
-        new BundleAnalyzerPlugin({
-            analyzerMode: 'static',
-            openAnalyzer: false
-        }),
         new CleanWebpackPlugin(),
+        new VueLoaderPlugin(),
         new HtmlWebpackPlugin({
             title: 'covidTimeSeries',
             template: './src/timeSeries/index.html',
-            filename: './dist/timeSeries/index.html',
-            inject: false
+            filename: './timeSeries/index.html',
         }),
-        new webpack.BannerPlugin(fs.readFileSync('./LICENSE', 'utf-8'))
+        new webpack.BannerPlugin(fs.readFileSync('./LICENSE', 'utf-8')),
     ],
     module: {
         rules: [
@@ -34,10 +29,6 @@ module.exports = {
                 test: /\.svg$/,
                 loader: 'svg-inline-loader'
 
-            },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
             },
             {
                 test: /\.(png|jpg|gif)$/i,
@@ -49,6 +40,10 @@ module.exports = {
                         },
                     },
                 ]
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
             }
         ]
     },
