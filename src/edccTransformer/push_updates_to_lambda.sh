0d5dcb1c0b26564a79843e1905e03a65abf7d689
Dylan Silke <38807135+s-i-l-k-e@users.noreply.github.com>
2020-04-15 10:37:11 +1000

    Adds lambda to transform data automatically once per day (#6)
    * Setup: initial commit of webpack setup
    
    * Chore: fixed location of covidTimeSeries script reference
    
    * Feat: adds transformer lambda to convert data and put into s3 bucket
    
    * Chore: fixes transformer, now working as lambda
    
    * Chore: reduce rate to 1x per day
    
    * Chore: removes way too many lines
    
    * Chore: removes way too many lines in transformer.yaml
    
    * Chore: improves file formatting
    
    * Fix: adds requirements.txt to make building package significantly easier
    
    * Chore: upload and overwrite most_recent data to make linking to distribution easier

---
+++ src/edccTransformer/push_updates_to_lambda.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+# Add packages to Python lambda distribution
+cd ./venv/lib/python3.6/site-packages/
+zip -r -9 ${OLDPWD}/transformer.zip .
+
+# Add transformer lambda code to Python distribution
+cd -
+zip -g transformer.zip transformer.py
+zip -g transformer.zip covid_spreadsheet.csv
+# Copy to source code bucket
+aws s3 cp ./transformer.zip s3://aristotle-lambdas/
+
+echo "Lambda changes uploaded"
