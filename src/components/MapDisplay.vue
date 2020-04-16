92c726ec3d75f4ffc1147a0afb6d49b92f1280c7
Francisco Gallardo <fragalcer@gmail.com>
2020-04-16 10:37:09 +1000

    Improved map-display component.

---
+++ src/components/MapDisplay.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="placeholder-bar-graph">
+        <GChart
+                type="GeoChart"
+                :data="mapData"
+                :options="chartOptions"
+                :settings="{ packages: ['geochart'],}"
+        />
+    </div>
+</template>
+
+<script>
+    import { GChart } from 'vue-google-charts'
+
+    export default {
+        name: 'MapDisplay',
+        components: {
+            GChart,
+        },
+        props: {
+            mapData: {
+                type: Array,
+                default: () => [],
+            }
+        },
+    }
+</script>
+
+<style>
+    .placeholder-bar-graph {
+        width: 70%;
+        height: 300px;
+        background-color: gray;
+    }
+
+    .placeholder-bar-graph p {
+        text-align: center;
+        padding-top: 100px;
+    }
+</style>
