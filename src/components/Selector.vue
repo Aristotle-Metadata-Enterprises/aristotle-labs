04900efa697f9db1b649f0508590bb234418b961
Harry White <harry.white424@gmail.com>
2020-04-16 14:07:02 +1000

    Fix selector v-model support

--- src/components/Selector.vue
+++ src/components/Selector.vue
@@ -3,11 +3,17 @@
         <label for="select-id">
             {{ description }}
         </label>
-        <select id="select-id" name="data element" v-model="selected" @change="updateMap(selected)">
+        <select
+            id="select-id"
+            name="data element"
+            v-model="selected"
+            @input="emitInput"
+            @change="updateMap(selected)"
+        >
             <option disabled value="">
                 {{ blankText }}
             </option>
-            <option v-for="o in options" :key="o.value" :value="o.value" :text="o.text">
+            <option v-for="o in options" :key="o.value" :value="o.value">
                 {{ o.text }}
             </option>
         </select>
@@ -15,7 +21,6 @@
 </template>
 
 <script>
-
     // Select form component with v-model support
     export default {
         props: {
@@ -39,6 +44,10 @@
             selected: "",
         }),
         methods: {
+            emitInput: function(event) {
+                this.$emit('input', event.target.value)
+            },
+            // TODO remove this event and have the map page use v-model
             updateMap: function(selectedOption) {
                 this.$emit('changeMap', selectedOption)
             },
