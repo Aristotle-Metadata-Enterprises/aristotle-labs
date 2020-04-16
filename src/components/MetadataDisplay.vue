99e19433ab9dd2286bb8f4a2c9100241f8b9f1be
Harry White <harry.white424@gmail.com>
2020-04-16 12:32:50 +1000

    Display filtered graph based on selections

--- src/components/MetadataDisplay.vue
+++ src/components/MetadataDisplay.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <p>Metadata display</p>
-        <svg class="metadata-display" ref="svg" width="1000" height="500">
+        <svg class="metadata-display" ref="svg" width="1500" height="500">
             <g />
         </svg>
     </div>
@@ -10,6 +10,8 @@
 <script>
 import * as d3 from 'd3'
 import dagreD3 from 'dagre-d3'
+
+import { mapPush } from '@/utils/mapping.js'
 
 export default {
     props: {
@@ -23,55 +25,98 @@ export default {
         }
     },
     computed: {
-        // Construct graphlib graph with whole dss structure
+        // Construct maps representing graph, used to contruct final filtered graph
         graph: function() {
-            let graph = new dagreD3.graphlib.Graph()
-            graph.setGraph({})
+            // Map of id to id of parents along with edge description
+            let parents = new Map()
+            // Map of node id's to node names and type
+            let nodeInfo = new Map()
 
             if (this.dss) {
                 // Create root dss node
-                graph.setNode(this.dss.uuid, {label: this.dss.name})
+                nodeInfo.set(
+                    this.dss.uuid,
+                    {name: this.dss.name, type: 'Dataset Specification'}
+                )
+                parents.set(this.dss.uuid, [])
                 for (let inc of this.dss.dssdeinclusionSet) {
-                    graph.setNode(inc.dataElement.uuid, {label: inc.dataElement.name})
-                    graph.setEdge(this.dss.uuid, inc.dataElement.uuid, {label: 'inclusion'})
+                    nodeInfo.set(
+                        inc.dataElement.uuid,
+                        {name: inc.dataElement.name, type: 'Data Element'}
+                    )
+                    mapPush(
+                        parents,
+                        inc.dataElement.uuid,
+                        {parent: this.dss.uuid, edgeLabel: 'inclusion'}
+                    )
                     for (let input of inc.dataElement.dedinputsthroughSet) {
-                        graph.setNode(
+                        nodeInfo.set(
                             input.dataElementDerivation.uuid,
-                            {label: input.dataElementDerivation.name}
+                            {name: input.dataElementDerivation.name, type: 'Data Element Derivation'}
                         )
-                        graph.setEdge(
-                            inc.dataElement.uuid,
+                        mapPush(
+                            parents,
                             input.dataElementDerivation.uuid,
-                            {label: 'input'}
+                            {parent: inc.dataElement.uuid, edgeLabel: 'input'}
                         )
                         for (let derive of input.dataElementDerivation.dedderivesthroughSet) {
-                            graph.setNode(
+                            nodeInfo.set(
                                 derive.dataElement.uuid,
-                                {label: derive.dataElement.name}
+                                {name: derive.dataElement.name, type: 'Data Element'}
                             )
-                            graph.setEdge(
-                                input.dataElementDerivation.uuid,
+                            mapPush(
+                                parents,
                                 derive.dataElement.uuid,
-                                {label: 'dervies'}
+                                {parent: input.dataElementDerivation.uuid, edgeLabel: 'derives'}
                             )
                         }
                     }
                 }
             }
-            return graph
+            return {parents: parents, nodeInfo: nodeInfo}
         },
-        // Return paths from nodes to source dss
-        sourcePaths: function() {
-            if (this.dss) {
-                return dagreD3.graphlib.alg.dijkstra(this.graph, this.dss.uuid)
-            }
-            return {}
-        }
     },
     watch: {
         // Compute dag based on selected
         selected: function(selected) {
-            let graph = this.graph
+            let graph = this.createDisplayGraph(selected)
+            this.drawGraph(graph)
+        }
+    },
+    methods: {
+        // Get a nodes label
+        getNodeLabel: function(uuid) {
+            return this.graph.nodeInfo.get(uuid).name
+        },
+        // Create dagre graph filtered to contain all paths to selected metadata
+        createDisplayGraph: function(selected) {
+            let dgraph = new dagreD3.graphlib.Graph()
+            dgraph.setGraph({})
+            let nodeStack = []
+
+            // Start with selected values
+            for (let val of selected) {
+                if (this.graph.nodeInfo.has(val)) {
+                    nodeStack.push(val)
+                    dgraph.setNode(val, {label: this.getNodeLabel(val)})
+                } else {
+                    console.error(`${val} is not a valid node`)
+                }
+            }
+
+            // While there are nodes to process add parents to graph
+            while (nodeStack.length > 0) {
+                let uuid = nodeStack.pop()
+                for (let parent of this.graph.parents.get(uuid)) {
+                    dgraph.setNode(parent.parent, {label: this.getNodeLabel(parent.parent)})
+                    dgraph.setEdge(parent.parent, uuid, {label: parent.edgeLabel})
+                    nodeStack.push(parent.parent)
+                }
+            }
+
+            return dgraph
+        },
+        drawGraph: function(graph) {
             let svg = d3.select(this.$refs.svg)
             let inner = svg.select('g')
             let render = new dagreD3.render()
@@ -106,7 +151,7 @@ svg.metadata-display {
 .metadata-display text {
   font-weight: 300;
   font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-  font-size: 14px;
+  font-size: 12px;
 }
 
 .metadata-display .node rect {
