dac4aea44e3587ab6adced38099a1767741ed906
Francisco Gallardo <fragalcer@gmail.com>
2020-04-17 15:08:15 +1000

    Implementing new filters.

---
+++ src/components/Filters.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="form-block">
+
+        <div v-for="o in options" :key="o">
+            <label :for="o">{{ o }}</label>
+            <input type="checkbox" :id="o" :value="o">
+        </div>
+
+<!--        <span>Checked names: {{  }}</span>-->
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Filters",
+        props: {
+            options: {
+                type: Array,
+                default: () => [],
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    .form-block {
+        display: block;
+        margin: 20px;
+    }
+</style>
