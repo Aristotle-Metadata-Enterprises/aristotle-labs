e1a301f58fa7cd5950f49bc4b31221c43e1249d7
Dylan Silke <d.ylan.s.silke@gmail.com>
2020-04-22 16:21:35 +1000

    Chore: adds bootstrap styling to GraphPage.vue

--- src/pages/covid/GraphPage.vue
+++ src/pages/covid/GraphPage.vue
@@ -1,106 +1,104 @@
 <template>
     <div class="covid-graph">
-        <h1>Aristotle Covid Graph</h1>
-        <div class="horizontal-container">
-            <div>
-                <selector 
-                    v-model="selected" 
-                    description="Choose a data element" 
-                    :options="options" 
-                />
-                <selector 
-                    v-model="selectedCategory" 
-                    description="Choose a category data element" 
-                    :options="categoryOptions"
-                />
+        <h1 class="text-center">COVID-19 Bar Chart</h1>
+        <div class="container padding-above-2x">
+            <div class="row">
+                <div class="col-sm-9">
+                    <bar-graph :selected="allSelected" :raw_data="raw_data" :distribution_map="distributionDataMap"/>
+                </div>
+                <div class="col-sm-3">
+                    <div class="card bg-light">
+                        <selector
+                                v-model="selected"
+                                description="Choose a data element"
+                                :options="options"
+                        />
+                        <selector
+                                v-model="selectedCategory"
+                                description="Choose a category data element"
+                                :options="categoryOptions"
+                        />
+                    </div>
+                </div>
             </div>
-            <bar-graph :selected="allSelected" :raw_data="raw_data" :distribution_map="distributionDataMap" />
+            <metadata-display :selected="allSelected" :dss="dss" tooltips class="padding-below"/>
         </div>
-        <metadata-display :selected="allSelected" :dss="dss" tooltips />
     </div>
 </template>
 
 <script>
-import Selector from '@/components/Selector.vue'
-import BarGraph from '@/components/BarGraph.vue'
-import MetadataDisplay from '@/components/MetadataDisplay.vue'
-import {
-    getCovidData,
-    getDistribution,
-    getDistributionOptions,
-    getDatasetSpecification,
-    mapDistributionData,
-    filterNumberDataElements,
-    filterValueDataElements,
-} from '@/data/covid.js'
+    import Selector from '@/components/Selector.vue'
+    import BarGraph from '@/components/BarGraph.vue'
+    import MetadataDisplay from '@/components/MetadataDisplay.vue'
+    import {
+        getCovidData,
+        getDistribution,
+        getDistributionOptions,
+        getDatasetSpecification,
+        mapDistributionData,
+        filterNumberDataElements,
+        filterValueDataElements,
+    } from '@/data/covid.js'
 
-export default {
-    data: () => ({
-        distribution: {},
-        dss: {},
-        raw_data: {},
-        selected: '',
-        selectedCategory: '',
-        options: [],
-        categoryOptions: [],
-        dataMap: new Map(),
-        distributionDataMap: {},
-    }),
-    components: {
-        'selector': Selector,
-        'bar-graph': BarGraph,
-        'metadata-display': MetadataDisplay,
-    },
-    mounted: function() {
-        getCovidData().then((raw_data) => {
-            this.raw_data = raw_data;
-        }).catch((error) =>
-        {
-            // TODO: handle errors gracefully
-            console.log(error)
-        });
-        getDistribution().then((data) => {
-            this.distribution = data;
-            this.options = getDistributionOptions(data, filterNumberDataElements);
-            this.categoryOptions = getDistributionOptions(data, filterValueDataElements);
-            this.distributionDataMap = mapDistributionData(data)
-        }).catch((error) => {
-            // TODO handle errors gracefully
-            console.error(error)
-        })
+    export default {
+        data: () => ({
+            distribution: {},
+            dss: {},
+            raw_data: {},
+            selected: '',
+            selectedCategory: '',
+            options: [],
+            categoryOptions: [],
+            dataMap: new Map(),
+            distributionDataMap: {},
+        }),
+        components: {
+            'selector': Selector,
+            'bar-graph': BarGraph,
+            'metadata-display': MetadataDisplay,
+        },
+        mounted: function () {
+            getCovidData().then((raw_data) => {
+                this.raw_data = raw_data;
+            }).catch((error) => {
+                // TODO: handle errors gracefully
+                console.log(error)
+            });
+            getDistribution().then((data) => {
+                this.distribution = data;
+                this.options = getDistributionOptions(data, filterNumberDataElements);
+                this.categoryOptions = getDistributionOptions(data, filterValueDataElements);
+                this.distributionDataMap = mapDistributionData(data)
+            }).catch((error) => {
+                // TODO handle errors gracefully
+                console.error(error)
+            })
 
-        getDatasetSpecification().then((data) => {
-            this.dss = data
-        }).catch((error) => {
-            // TODO handle errors gracefully
-            console.error(error)
-        })
-    },
-    computed: {
-        allSelected: function() {
-            return [this.selected, this.selectedCategory]
+            getDatasetSpecification().then((data) => {
+                this.dss = data
+            }).catch((error) => {
+                // TODO handle errors gracefully
+                console.error(error)
+            })
+        },
+        computed: {
+            allSelected: function () {
+                return [this.selected, this.selectedCategory]
+            }
         }
     }
-}
 </script>
 
 <style>
-h1 {
-    border-bottom: 1px solid black;
-}
+    .padding-above {
+        margin-top: 20px;
+    }
 
-.root {
-    display: flex;
-    flex-direction: column;
-}
+    .padding-above-2x {
+        margin-top: 40px;
+    }
 
-.horizontal-container {
-    display: flex;
-    flex-direction: row;
-}
-
-.placeholder-metadata {
-    margin-top: 50px;
-}
+    .padding-below {
+        margin-bottom: 20px;
+    }
 </style>
-k
