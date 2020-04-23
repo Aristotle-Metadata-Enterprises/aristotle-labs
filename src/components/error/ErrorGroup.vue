50689fbad9045d4b5636babfd489a2b7607c72fc
Harry White <harry.white424@gmail.com>
2020-04-23 15:04:26 +1000

    Make error display more robust
    Handle regular error objects, don't show duplicates

--- src/components/error/ErrorGroup.vue
+++ src/components/error/ErrorGroup.vue
@@ -1,21 +1,35 @@
 <template>
     <div class="error-group">
-        <error v-for="error in errors" :key="error" :error="error" />
+        <div v-for="message in messages" :key="message" class="alert alert-danger">
+            {{ message }}
+        </div>
     </div>
 </template>
 
 <script>
-import Error from '@/components/error/Error.vue'
+import { NiceError } from '@/error/class.js'
 
 export default {
-    components: {
-        'error': Error
-    },
     props: {
         // An array of NiceError objects to display
         errors: {
             type: Array,
             required: true,
+        }
+    },
+    computed: {
+        messages: function() {
+            // use a set to avoid duplicate messages
+            let messages = new Set()
+            for (let error of this.errors) {
+                if (error instanceof NiceError) {
+                    messages.add(error.message)
+                } else {
+                    // Use generic message if we didnt get a NiceError
+                    messages.add('An error has occurred')
+                }
+            }
+            return messages
         }
     }
 }
