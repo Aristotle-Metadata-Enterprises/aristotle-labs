0f906c2f9ece5365c82e1c38a85c4fe6f5f2830c
Dylan Silke <d.ylan.s.silke@gmail.com>
2020-04-23 14:56:56 +1000

    Chore: further improvements to the styling

--- src/components/Selector.vue
+++ src/components/Selector.vue
@@ -2,51 +2,60 @@
     <div class="form-block">
         <p class="font-weight-bold">{{ description }}</p>
         <div class="form-group">
-            <template v-for="o in options">
-                <label :for="o.value">{{ o.text }}</label>
-                <input type="radio" :key="o.value" :value="o.value" :id="o.value" @input="emitInput">
-            </template>
+            <ul v-for="o in options">
+                <li class="no-dots">
+                    <input type="radio" :key="o.value" :value="o.value" :id="o.value" @input="emitInput">
+                    <label :for="o.value">{{ o.text }}</label>
+                </li>
+            </ul>
         </div>
     </div>
 </template>
 
 <script>
 
-// Select form component with v-model support
-export default {
-    props: {
-        // Selected value
-        value: {
-            type: String,
-            default: '',
+    // Select form component with v-model support
+    export default {
+        props: {
+            // Selected value
+            value: {
+                type: String,
+                default: '',
+            },
+            // Description of selection
+            description: {
+                type: String,
+                required: true,
+            },
+            // Text to display when empty
+            blankText: {
+                type: String,
+                default: '----------',
+            },
+            // Options as an array of objects with value and text properties
+            options: {
+                type: Array,
+                default: () => [],
+            }
         },
-        // Description of selection
-        description: {
-            type: String,
-            required: true,
-        },
-        // Text to display when empty
-        blankText: {
-            type: String,
-            default: '----------',
-        },
-        // Options as an array of objects with value and text properties
-        options: {
-            type: Array,
-            default: () => [],
+        methods: {
+            emitInput: function (event) {
+                this.$emit('input', event.target.value)
+            },
         }
-    },
-    methods: {
-        emitInput: function (event) {
-            this.$emit('input', event.target.value)
-        },
     }
-}
 </script>
 
 <style scoped>
+    .no-dots {
+        list-style-type: none;
+    }
     .form-block {
         display: block;
         margin: 20px;
+    }
+
+    input[type=radio] {
+        transform: scale(1.5);
     }
 </style>
