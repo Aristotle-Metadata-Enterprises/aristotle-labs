ee697193a1b62036eaac011b74e96020aca99106
Francisco Gallardo <fragalcer@gmail.com>
2020-04-23 21:12:12 +1000

    Added initialised options for checkboxes.

--- src/components/CheckboxSection.vue
+++ src/components/CheckboxSection.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class="form-block form-check">
-        <span :data-aristotle-concept-id="id">{{ name }}</span>
-        <div v-for="o in options" :key="o.id">
-            <input type="checkbox" class="form-check-input" :id="o.id" :value="o.name" @change="updateCheckedOptions" v-model="checkedOptions">
-            <label :for="o.id" class="form-check-label">{{ o.name }}</label>
+    <div>
+        <strong class="pl-20" :data-aristotle-concept-id="id">{{ name }}</strong>
+        <div class="form-block form-check">
+            <div v-for="o in options" :key="o.id">
+                <input type="checkbox" class="form-check-input" :id="o.id" :value="o.name" @change="updateCheckedOptions" v-model="checkedOptions">
+                <label :for="o.id" class="form-check-label">{{ o.name }}</label>
+            </div>
         </div>
     </div>
 </template>
@@ -38,11 +40,18 @@ export default {
             'longDefinitionWords': 75,
             'placement': 'top',
         });
+        this.initialiseOptions()
     },
     methods: {
         updateCheckedOptions: function () {
             this.$emit('updateCheckedOpt', this.checkedOptions, this.name)
         },
+        initialiseOptions: function () {
+            for(let option of this.options) {
+                this.checkedOptions.push(option.name)
+            }
+            this.updateCheckedOptions()
+        }
     }
 }
 </script>
@@ -50,6 +59,11 @@ export default {
 <style scoped>
 .form-block {
     display: block;
-    margin: 20px;
+    margin-left: 20px;
+    margin-right: 20px;
+    margin-bottom: 20px;
+}
+.pl-20 {
+    padding-left: 20px;
 }
 </style>
