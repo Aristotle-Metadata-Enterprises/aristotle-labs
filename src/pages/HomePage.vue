35b188cdec41363f6c2bbb8ddd098cb78c6bf386
Sam Spencer <theodore.therone@gmail.com>
2020-05-02 17:36:20 +1000

    Fix warped logo, improve home layout

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -10,50 +10,57 @@
                     Metadata isn't just about describing data, it's about <em>empowering data</em>
                     to unlock its full potential.
                 </p>
-                <hr class="my-4">
+                <hr class="m-4">
                 <p>
                     Below are a few of the projects built on the <a href="https://aristotlemetadata.com">Aristotle
                     Metadata Registry</a>
                     that demonstrate how a metadata registry makes data make sense.
                 </p>
                 <p class="lead">
-                    <a class="btn btn-primary btn-lg" href="https://aristotlemetadata.com" role="button">
-                        Learn more &raquo;
+                    <a class="btn btn-outline-dark" href="https://aristotlemetadata.com" role="button">
+                        Learn more about the Aristotle Metadata Registry &raquo;
                     </a>
                 </p>
             </div>
         </div>
         <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <h2>COVID-19 World Map</h2>
-                    <p>
-                        Using authoritative COVID-19 data from the ECDC and the WHO, use metadata selections to view
-                        a world map of COVID-19 data. You can also press play to view the progression of the pandemic
-                        across the globe.
-                    </p>
-                    <p>
-                        <router-link class="btn btn-secondary" :to="{name: 'covidMap'}">
-                            Covid Map &raquo;
+            <div class="card-deck mb-3">
+                <div class="card">
+                    <img class="card-img-top" src="@/assets/landing-covid-bar.png" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">COVID-19 Bar chart</h5>
+                        <p class="card-text">
+                        View data from the European Center for Disease Control and the World Health Organisation,
+                            use metadata selections to examine a bar chart of COVID-19 Data. 
+                            There is also a metadata display to discover exactly how the data was collected and collated.
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <router-link class="btn btn-primary" :to="{name: 'covidGraph'}">
+                            See the COVID Graph &raquo;
                         </router-link>
-                    </p>
+                    </div>
                 </div>
-                <div class="col-md-6">
-                    <h2>COVID-19 Bar Chart</h2>
-                    <p>
-                        Using authoritative COVID-19 data from the ECDC and the WHO, use metadata selections to view
-                        a bar chart of COVID-19 Data. There is also a metadata display to discover exactly how the
-                        data is collected and collated.
-                    </p>
-                    <p>
-                        <router-link class="btn btn-secondary" :to="{name: 'covidGraph'}">
-                            Covid Graph &raquo;
+                <div class="card">
+                    <img class="card-img-top" src="@/assets/landing-covid-map.png" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">COVID-19 Map chart</h5>
+                        <p class="card-text">
+                        View an interactive map based on COVID-19 data from the European Center for Disease Control and the World Health Organisation.
+                            This dashboard includes Aristotle Metadata that provides additional context to data and an interactive time slider to view the progression of the pandemic across the globe.
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <router-link class="btn btn-primary" :to="{name: 'covidMap'}">
+                            See the COVID Map &raquo;
                         </router-link>
-                    </p>
+                    </div>
                 </div>
             </div>
-            <hr>
         </div>
+        <hr class="m-5" />
+
+
     </div>
 </template>
 
@@ -62,4 +69,12 @@ export default {}
 </script>
 
 <style scoped>
+.card-img-top {
+    width: 100%;
+    height: 28vw;
+    object-fit: cover;
+}
+.card-body {
+    border-top: 1px solid lightgray;
+}
 </style>
