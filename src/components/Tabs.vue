7f4c8f28a952f0c44e47c6c3974d244d354dcff3
Francisco Gallardo <fragalcer@gmail.com>
2020-05-05 10:40:15 +1000

    Implemented tabs.

---
+++ src/components/Tabs.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link" :class="{ 'active': !graphTabActive}" href="#/covid/map">Map view</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" :class="{ 'active': graphTabActive}" href="#/covid/graph">Graph view</a>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: "Tabs",
+    props: {
+        graphTabActive: {
+            type: Boolean,
+            default: false,
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
