561e995972d072e2be53bb219d03e4c59cabeea5
Dylan Silke <d.ylan.s.silke@gmail.com>
2020-06-03 11:45:02 +1000

    Chore: removes name of aristotle lambdas bucket

--- src/ecdcTransformer/push_updates_to_lambda.sh
+++ src/ecdcTransformer/push_updates_to_lambda.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+BUCKET_NAME=$1
 set -e
 # Add packages to Python lambda distribution
 cd ./venv/lib/python3.6/site-packages/
@@ -9,6 +10,6 @@ cd -
 zip -g transformer.zip transformer.py
 zip -g transformer.zip covid_spreadsheet.csv
 # Copy to source code bucket
-aws s3 cp ./transformer.zip s3://aristotle-lambdas/
+aws s3 cp ./transformer.zip s3://BUCKET_NAME
 
 echo "Lambda changes uploaded"
